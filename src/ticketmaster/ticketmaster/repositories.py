759a1ed3dc508aa319665aab8a3f33708cb0f08e
Aleksei Sosov <forgithublesh@yandex.ru>
2026-04-18 16:41:50 +0400

    [ticketmaster] chore repositories.py adopt sqlmodel.select and session.exec

--- src/ticketmaster/ticketmaster/repositories.py
+++ src/ticketmaster/ticketmaster/repositories.py
@@ -1,5 +1,5 @@
-from sqlalchemy import select
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from ticketmaster.models import Event
 from ticketmaster.schemas.dtos import EventDTO
@@ -8,5 +8,5 @@ from ticketmaster.schemas.dtos import EventDTO
 class EventRepository:
     @classmethod
     async def get_all(cls, session: AsyncSession) -> list[EventDTO]:
-        result = await session.execute(select(Event))
-        return [EventDTO.from_sqlmodel(event) for event in result.scalars().all()]
+        result = await session.exec(select(Event))
+        return [EventDTO.from_sqlmodel(event) for event in result.all()]
