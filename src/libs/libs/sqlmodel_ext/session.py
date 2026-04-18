5e693709019ccfa4e63e6f0a27268495b1221e29
Aleksei Sosov <forgithublesh@yandex.ru>
2026-04-18 16:41:46 +0400

    [libs] chore sqlmodel_ext/session.py wired sqlmodel AsyncSession into session factory

--- src/libs/libs/sqlmodel_ext/session.py
+++ src/libs/libs/sqlmodel_ext/session.py
@@ -1,3 +1,4 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlmodel.ext.asyncio.session import AsyncSession
 
-Session = async_sessionmaker(autobegin=False)
+Session = async_sessionmaker(class_=AsyncSession, autobegin=False)
