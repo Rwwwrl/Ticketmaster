7c00568cd2d98004ab407f7716366824362f423e
Aleksei Sosov <forgithublesh@yandex.ru>
2026-04-23 21:18:18 +0400

    [ticketmaster] feat hello/main.py added /ping route

--- src/hello/hello/main.py
+++ src/hello/hello/main.py
@@ -11,3 +11,8 @@ async def root() -> dict[str, str]:
 @app.get("/health")
 async def health() -> dict[str, str]:
     return {"status": "ok"}
+
+
+@app.get("/ping")
+async def ping() -> dict[str, str]:
+    return {"message": "pong"}
