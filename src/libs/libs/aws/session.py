d7ba28c6e8a5c96b03abd56ed27b79f1ac8bfce4
Aleksei Sosov <forgithublesh@yandex.ru>
2026-05-11 16:02:32 +0400

    [libs] feat aws/session.py made credentials and session token optional in bind_task_role_to_aws_session

--- src/libs/libs/aws/session.py
+++ src/libs/libs/aws/session.py
@@ -3,6 +3,18 @@ import aioboto3
 aws_session = aioboto3.Session()
 
 
-def bind_task_role_to_aws_session(*, region: str, access_key_id: str, secret_access_key: str) -> None:
-    aws_session._session.set_credentials(access_key_id, secret_access_key)
+def bind_task_role_to_aws_session(
+    *,
+    region: str,
+    access_key_id: str | None,
+    secret_access_key: str | None,
+    session_token: str | None,
+) -> None:
+    if access_key_id is not None and secret_access_key is not None:
+        aws_session._session.set_credentials(
+            access_key=access_key_id,
+            secret_key=secret_access_key,
+            token=session_token,
+        )
+
     aws_session._session.set_config_variable("region", region)
