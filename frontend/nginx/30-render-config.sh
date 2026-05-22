1cb815bd1fe2349c251d2a639f03ff877a75b295
Aleksei Sosov <forgithublesh@yandex.ru>
2026-05-22 21:48:46 +0400

    [frontend] chore nginx/30-render-config.sh removed set -e and env var presence guards

--- frontend/nginx/30-render-config.sh
+++ frontend/nginx/30-render-config.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-set -e
-: "${COGNITO_USER_POOL_ID:?}"
-: "${COGNITO_USER_POOL_CLIENT_ID:?}"
-: "${COGNITO_DOMAIN:?}"
 envsubst '${COGNITO_USER_POOL_ID} ${COGNITO_USER_POOL_CLIENT_ID} ${COGNITO_DOMAIN}' \
     < /etc/nginx/templates/config.js.template \
     > /usr/share/nginx/html/config.js
