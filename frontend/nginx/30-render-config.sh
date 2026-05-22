0253c4382b890821c82dc1e31dab2c06a37beadf
Aleksei Sosov <forgithublesh@yandex.ru>
2026-05-22 21:17:03 +0400

    [frontend] feat Dockerfile/nginx config injection plumbing for cognito values via nginx envsubst

---
+++ frontend/nginx/30-render-config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+envsubst '${COGNITO_USER_POOL_ID} ${COGNITO_USER_POOL_CLIENT_ID} ${COGNITO_DOMAIN}' \
+    < /etc/nginx/templates/config.js.template \
+    > /usr/share/nginx/html/config.js
